1e25f16ebf362c25096cbec79617dafe2abb36e6
Amit Kumar <amit.kumar09081991@gmail.com>
2018-05-06 00:01:05 +0530

    Added for auto-login

--- src/main/java/com/cwp/alice/rs/request/dto/Parameters.java
+++ src/main/java/com/cwp/alice/rs/request/dto/Parameters.java
@@ -2,9 +2,20 @@ package com.cwp.alice.rs.request.dto;
 
 public class Parameters
 {
-  private String param;
-
-  public String getParam() { return this.param; }
-
-  public void setParam(String param) { this.param = param; }
+	private String user_id;
+	private String cw_pwd;
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getCw_pwd() {
+		return cw_pwd;
+	}
+	public void setCw_pwd(String cw_pwd) {
+		this.cw_pwd = cw_pwd;
+	}
+  
 }
