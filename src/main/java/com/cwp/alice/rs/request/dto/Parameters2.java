eaf17350d7d26377f48678e911f68029f09ab98c
Amit Kumar <amit.kumar09081991@gmail.com>
2018-05-05 23:54:27 +0530

    Adding changes for auto-login

--- src/main/java/com/cwp/alice/rs/request/dto/Parameters2.java
+++ src/main/java/com/cwp/alice/rs/request/dto/Parameters2.java
@@ -1,5 +1,20 @@
 package com.cwp.alice.rs.request.dto;
 
 public class Parameters2 {
-
+	private String user_id;
+	private String cw_pwd;
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getCw_pwd() {
+		return cw_pwd;
+	}
+	public void setCw_pwd(String cw_pwd) {
+		this.cw_pwd = cw_pwd;
+	}
+	
 }
