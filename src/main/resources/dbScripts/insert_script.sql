3e24709e0c5ef20998f1392b415c8ad4bf87e46e
Luv <javachemistry@gmail.com>
2018-05-18 12:37:53 +0530

    Update insert_script.sql
    Changed email id

--- src/main/resources/dbScripts/insert_script.sql
+++ src/main/resources/dbScripts/insert_script.sql
@@ -12,7 +12,7 @@ INSERT INTO cw_users
        ('Luv Gupta'
             , 101
             , '$2a$10$RAcfBB1fd7CJd5srLJ7ZdegECOdhuhAkzC.saDentBVjx8otRPvs.'
-            , 'lugupta@deloitte.com'
+            , 'test1@gmail.com'
             , 'Case Analyst'
             , 'Assistant Secretary for Health'
             , 'US Health & Human Services'
@@ -34,7 +34,7 @@ INSERT INTO cw_users
        ('Amit Kumar'
             , 102
             , '$2a$10$.suLp/nK2wFDmw/0IcpyTe.naDmstgzf5jO6OXlYV5nc.QFJJXnsO'
-            , 'amitkumar466@deloitte.com'
+            , 'test2@gmail.com'
             , 'Case Analyst'
             , 'Assistant Secretary for Human Services'
             , 'US Health & Human Services'
