1c5ea667101b39f1553bf810b1d21b008e477608
Amit Kumar <amit.kumar09081991@gmail.com>
2018-05-21 21:57:55 +0530

    Added

--- src/main/webapp/js/factory.js
+++ src/main/webapp/js/factory.js
@@ -24,46 +24,41 @@ function Factory() {
         return factoryObject;
     }
 }
-    
+ 
+function fetchChatBotHistory(){
+	var currentConvObj = [];
+	$('.app-content .item-container,.app-content .time-indicator').each(function( index ) {
+	  if($(this).hasClass('item-container-bot')){
+		console.log( index + " Bot: " + $( this ).text() );
+		item = {};
+        item ["message"] = $( this ).text();
+        item ["messageType"] = "ITEM_BOT";
+        item ["messageDataType"] = "TEXT";
+        currentConvObj.push(item);
+	  } else if($(this).hasClass('item-container-user')){
+		console.log( index + " User: " + $( this ).text() );
+		item = {};
+        item ["message"] = $( this ).text();
+        item ["messageType"] = "TIME_INDICATOR";
+        item ["messageDataType"] = "TEXT";
+        currentConvObj.push(item);
+	  } else if($(this).hasClass('time-indicator')){
+		console.log( index + " Time: " + $( this ).text() );
+		item = {};
+        item ["message"] = $( this ).text();
+        item ["messageType"] = "ITEM_USER";
+        item ["messageDataType"] = "DATE";
+        currentConvObj.push(item);
+	  }				  
+	});
+	return currentConvObj;
+}
+
 var CustomCaseFilter = function () {
 	return {
         execute: function (actingObject) { 
 			console.log('Executing for display custom case filter logic');
-			
-		}
-    }
-};
-
-var OwnerCaseFilter = function () {
-	return {
-        execute: function (actingObject) { 
-			console.log('Executing for display owner case filter logic');
-			var currentConvObj = [];
-			$('.app-content .item-container,.app-content .time-indicator').each(function( index ) {
-			  if($(this).hasClass('item-container-bot')){
-				console.log( index + " Bot: " + $( this ).text() );
-				item = {};
-		        item ["message"] = $( this ).text();
-		        item ["messageType"] = "ITEM_BOT";
-		        item ["messageDataType"] = "TEXT";
-		        currentConvObj.push(item);
-			  } else if($(this).hasClass('item-container-user')){
-				console.log( index + " User: " + $( this ).text() );
-				item = {};
-		        item ["message"] = $( this ).text();
-		        item ["messageType"] = "TIME_INDICATOR";
-		        item ["messageDataType"] = "TEXT";
-		        currentConvObj.push(item);
-			  } else if($(this).hasClass('time-indicator')){
-				console.log( index + " Time: " + $( this ).text() );
-				item = {};
-		        item ["message"] = $( this ).text();
-		        item ["messageType"] = "ITEM_USER";
-		        item ["messageDataType"] = "DATE";
-		        currentConvObj.push(item);
-			  }				  
-			});
-			
+			var currentConvObj = fetchChatBotHistory();
 			$.ajax({
 				method: "POST",
 				url: "/CaseWorkerPortal/saveAliceConversation",
@@ -72,8 +67,35 @@ var OwnerCaseFilter = function () {
 			})
 			.done(function( msg ) {
 				console.log("success when alice conversation object created");
-				// Add filter post successful navigation
-				$('#inputTabularData_filter input[type="search"]').val(actingObject).triggerHandler('keydown');
+				window.location.href = '/CaseWorkerPortal/cwDashboard';
+				
+				
+			});
+		}
+    }
+};
+
+var OwnerCaseFilter = function () {
+	return {
+        execute: function (actingObject) { 
+			console.log('Executing for display owner case filter logic');
+			var currentConvObj = fetchChatBotHistory();
+			$.ajax({
+				method: "POST",
+				url: "/CaseWorkerPortal/saveAliceConversation",
+				data: JSON.stringify(currentConvObj),
+				contentType:"application/json"
+			})
+			.done(function( msg ) {
+				console.log("success when alice conversation object created");
+				window.location.href = '/CaseWorkerPortal/cwDashboard';
+				/*$.ajax({
+					method: "GET",
+					url: "/CaseWorkerPortal/cwDashboard"
+				})
+				.done(function( msg ) {
+					$('#inputTabularData_filter input[type="search"]').val(actingObject).triggerHandler('keydown');
+				});*/
 			});
 		}
     }
@@ -83,7 +105,19 @@ var DisplayCaseFilter = function () {
     return {
         execute: function (actingObject) { 
 			console.log('Executing for display case filter logic');
+			var currentConvObj = fetchChatBotHistory();
+			$.ajax({
+				method: "POST",
+				url: "/CaseWorkerPortal/saveAliceConversation",
+				data: JSON.stringify(currentConvObj),
+				contentType:"application/json"
+			})
+			.done(function( msg ) {
+				console.log("success when alice conversation object created");
+				window.location.href = '/CaseWorkerPortal/openCase/'+actingObject;
+			});
 			
+			//Add final outcome to chat interface
 		}
     }
 };
@@ -92,7 +126,19 @@ var ViewAppointmentsFilter = function () {
     return {
         execute: function (actingObject) { 
 			console.log('Executing for view appointment filter logic');
-			
+			var currentConvObj = fetchChatBotHistory();
+			$.ajax({
+				method: "POST",
+				url: "/CaseWorkerPortal/saveAliceConversation",
+				data: JSON.stringify(currentConvObj),
+				contentType:"application/json"
+			})
+			.done(function( msg ) {
+				console.log("success when alice conversation object created");
+				window.location.href = '/CaseWorkerPortal/cwAppointment';
+			});
+						
+			//Add final outcome to chat interface
 		}
     }
 };
