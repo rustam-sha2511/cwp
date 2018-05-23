7a01752892cadc314e3dcd201f68b862f3a17c72
Amit Kumar <amit.kumar09081991@gmail.com>
2018-05-23 06:31:27 +0530

    Added for Case Creation logic

--- src/main/java/com/cwp/alice/rs/request/dto/Parameters2.java
+++ src/main/java/com/cwp/alice/rs/request/dto/Parameters2.java
@@ -3,6 +3,11 @@ package com.cwp.alice.rs.request.dto;
 public class Parameters2 {
 	private String user_id;
 	private String cw_pwd;
+	private String case_id;
+	private String pa_name;
+	private String adult_count;
+	private String children_count;
+	private String monthly_income; 
 	
 	public String getUser_id() {
 		return user_id;
@@ -15,6 +20,36 @@ public class Parameters2 {
 	}
 	public void setCw_pwd(String cw_pwd) {
 		this.cw_pwd = cw_pwd;
+	}
+	public String getCase_id() {
+		return case_id;
+	}
+	public void setCase_id(String case_id) {
+		this.case_id = case_id;
+	}
+	public String getPa_name() {
+		return pa_name;
+	}
+	public void setPa_name(String pa_name) {
+		this.pa_name = pa_name;
+	}
+	public String getAdult_count() {
+		return adult_count;
+	}
+	public void setAdult_count(String adult_count) {
+		this.adult_count = adult_count;
+	}
+	public String getChildren_count() {
+		return children_count;
+	}
+	public void setChildren_count(String children_count) {
+		this.children_count = children_count;
+	}
+	public String getMonthly_income() {
+		return monthly_income;
+	}
+	public void setMonthly_income(String monthly_income) {
+		this.monthly_income = monthly_income;
 	}
 	
 }
