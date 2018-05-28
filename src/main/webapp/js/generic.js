951b241de113c6b0085efe04d9ead84779e9266f
Amit Kumar <amit.kumar09081991@gmail.com>
2018-05-28 23:12:43 +0530

    Starting Alice on trigger invocation

--- src/main/webapp/js/generic.js
+++ src/main/webapp/js/generic.js
@@ -1,17 +1,29 @@
 // Initialize Variables
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function(event) {
 	var button = $("#voice-icon");
 	var overlay = $("#overlay");
     var popup = $("#popup");
     var minimize = $("#minimizeAlice");
+    var close = $("#closeAlice");
+    
 	$(button).on('click', function(){
 		$(overlay).css('display','block');
         $(popup).css('display','block');
+        if($(".time-indicator-content").text() === undefined || $(".time-indicator-content").text() === ""){
+        	triggerInitialConversation();
+        }        
 	});
 	
 	$(minimize).on('click', function(){
 		$(overlay).css('display','none');
         $(popup).css('display','none');
         $(button).css('display','block');
+	});
+	
+	$(close).on('click', function(){
+		$(overlay).css('display','none');
+        $(popup).css('display','none');
+        $(button).css('display','block');
+        $('.app-content').html('<div class="time-indicator"><div class="time-indicator-content"></div><hr /></div>');        
 	});
 });
