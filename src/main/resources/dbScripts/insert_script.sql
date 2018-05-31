e02a24d20fd1cefb1e4bd5cbebdb721cb8f284f4
Amit Kumar <amit.kumar09081991@gmail.com>
2018-05-31 21:03:24 +0530

    Commit for invalid script

--- src/main/resources/dbScripts/insert_script.sql
+++ src/main/resources/dbScripts/insert_script.sql
@@ -252,7 +252,7 @@ INSERT INTO cw_cases
        VALUES
        (101
             , '04/13/2018'
-            , '{"pt":"TANF","hoh":"John Doe","adultCnt":"2","childrenCnt":"2","monthlyIncome":"$150","assistanceElig":"$100"}'
+            , '{"pt":"TANF","hoh":"John Doe","adultCnt":"2","childrenCnt":"2","monthlyIncome":"$150","assistanceElig":"$150"}'
             , 'Pending Review'
             , 'Luv Gupta'
        )
@@ -268,7 +268,7 @@ INSERT INTO cw_cases
        VALUES
        (101
             , '04/16/2018'
-            , '{"pt":"TANF","hoh":"Jane Doe","adultCnt":"1","childrenCnt":"3","monthlyIncome":"$100","assistanceElig":"$175"}'
+            , '{"pt":"TANF","hoh":"Jane Doe","adultCnt":"1","childrenCnt":"3","monthlyIncome":"$150","assistanceElig":"$175"}'
             , 'Approved'
             , 'Luv Gupta'
        )
@@ -300,7 +300,7 @@ INSERT INTO cw_cases
        VALUES
        (102
             , '04/13/2018'
-            , '{"pt":"TANF","hoh":"Robert Brown","adultCnt":"3","childrenCnt":"6","monthlyIncome":"$500","assistanceElig":"$100"}'
+            , '{"pt":"TANF","hoh":"Robert Brown","adultCnt":"3","childrenCnt":"6","monthlyIncome":"$500","assistanceElig":"$150"}'
             , 'Pending Review'
             , 'Amit Kumar'
        )
