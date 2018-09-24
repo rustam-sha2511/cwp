9603192ec58e32a99115e663c91530a907c2effb
Amit Kumar <amit.kumar09081991@gmail.com>
2018-09-25 01:02:29 +0530

    Added for ALICE v2.0

---
+++ src/main/java/com/cwp/alice/dto/FaqResponse.java
@@ -0,0 +1,27 @@
+package com.cwp.alice.dto;
+
+public class FaqResponse {
+	private String title;
+	private String link;
+	private String nid;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getNid() {
+		return nid;
+	}
+	public void setNid(String nid) {
+		this.nid = nid;
+	}
+	
+}
